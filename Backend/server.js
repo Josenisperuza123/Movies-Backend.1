750f69bb32da5900ee44f11417d02754b3a78e2a
Josenisperuza123 <jose.nisperuza@est.iudigital.edu.co>
2025-04-04 16:17:06 -0500

    .--

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,28 @@
-    const app = require('./app')
+const express = require('express')
+const { getConnection } = require('./db/connect-mongo')
+const cors = require('cors');
+require('dotenv').config()
+  
+  
+const app = express()
+const port = process.env.PORT;
+  
+app.use(cors());
+  
+getConnection();
+  
+app.use(express.json());
+  
+app.use('/productora', require('./Router/ProductoraRou'));
+app.use('/director', require('./Router/DirectorRou'));
+app.use('/genero', require('./Router/GeneroRou'));
+app.use('/tipo', require('./Router/TipoRou'));
+app.use('/media', require('./Router/MediaRou'));
 
-    app.set('port', process.env.PORT || 5500) //Error en esta ruta
-
-    app.get('*', (req, res) => {
-        return res.status(404).json({msj: 'No found'})
-    })
-
-    // Iniciar el servidor
-    app.listen(app.get ('port'), () => {
-        console.log(`Servidor corriendo en el puerto: ${app.get('port')}`)
-    })
+app.get('/', (req, res) => {
+  res.send('servidor funcionando');
+}),
+  
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
